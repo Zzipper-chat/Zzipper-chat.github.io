b105e36eaa6ecae5966f234461d80bbcc621674f
thatrandomperson5 <thatrandomperson5@users.noreply.github.com>
2022-09-02 20:44:48 +0000

    :art: Format code with prettier

--- web/resources/app/index.js
+++ web/resources/app/index.js
@@ -1,9 +1,9 @@
-if (typeof Cookies.get("session-name") !== 'undefined') {
+if (typeof Cookies.get("session-name") !== "undefined") {
   window.location.replace("./name");
 } else {
-var session_named = document.getElementsByClassName("session-name-here");
-for (var i = 0; i < slides.length; i++) {
-   var item = slides.item(i)
-   item.textContent = Cookies.get("session-name")
-}
+  var session_named = document.getElementsByClassName("session-name-here");
+  for (var i = 0; i < slides.length; i++) {
+    var item = slides.item(i);
+    item.textContent = Cookies.get("session-name");
+  }
 }
