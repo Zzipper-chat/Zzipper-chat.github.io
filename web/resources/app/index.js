b19b7a9a03adcff4d7c8c694615ec97e90362165
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-02 15:46:45 -0500

    Update index.js

--- web/resources/app/index.js
+++ web/resources/app/index.js
@@ -1,8 +1,9 @@
 if (typeof Cookies.get("session-name") !== "undefined") {
+  console.log("RE....");
   window.location.replace("./name");
 } else {
   var session_named = document.getElementsByClassName("session-name-here");
-  for (var i = 0; i < slides.length; i++) {
+  for (var i = 0; i < session_named.length; i++) {
     var item = slides.item(i);
     item.textContent = Cookies.get("session-name");
   }
