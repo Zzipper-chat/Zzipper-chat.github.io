462cf9e0dd349d1e28da22426dc9157e5ca81df6
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-11-25 18:54:35 +0000

    Hmmmm

--- web/resources/app/connections.js
+++ web/resources/app/connections.js
@@ -5,8 +5,18 @@ let ConnControl = {
   name: null,
 };
 const ConnModify = new Event("ConnModify");
-function ModifConn(host) {
+function ModifConn(host, auth) {
   let ws = new WebSocket(`wss://${host}/ws`);
+  ws.onopen = () => {
+    ws.send(
+      JSON.stringify(
+        {
+          datetime: new Date().toLocaleString(), 
+          auth: auth
+        }
+        )
+    )
+  }
   ConnControl.host = `https://${host}`;
   ConnControl.socketobj = ws;
   fetch(`${ConnControl.host}/info`)
